a1f17f8579e9b74332cc71a1df8a9480b8e04313
Philipp Fock <philipp.fock@salamantex.com>
2020-05-31 13:08:44 +0200

    better typing for entities and helper functions for creation

---
+++ src/entities/persons.ts
@@ -0,0 +1,61 @@
+import { v4 as uuidv4 } from 'uuid';
+import { IWorkPlace, workplaces } from './workplaces';
+
+/* Note to self:
+ * - this is suboptimal, bc I need to type the same thing again as in the graphql schema and it can easily diverge 
+ */
+
+const names = ['Jeff', 'Bill', 'Gustav', 'Franco', 'Christina', 'Kristine', 'Bella', 'Joseph', 'Maria'];
+
+export interface IPerson {
+    id: string;
+    name: string;
+}
+
+export interface IStudent extends IPerson {
+    school: string;
+}
+
+export type BaseStudent = Omit<IStudent, "id">;
+
+export function newStudent(baseStudent: BaseStudent): IStudent {
+    return {
+        ...baseStudent,
+        id: uuidv4(),
+    };
+}
+
+interface IWorker extends IPerson {
+    workplace?: IWorkPlace;
+}
+
+export function newWorker(name: string, workplaceID: string): IWorker {
+    const workplace = workplaces.find(workplace => workplace.id === workplaceID);
+    return {
+        name,
+        workplace,
+        id: uuidv4()
+    }
+}
+
+export type WorkerOrStudent = IWorker | IStudent;
+
+export function isStudent(person: WorkerOrStudent): person is IStudent {
+    return !!(person as IStudent).school;
+}
+
+export const persons: WorkerOrStudent[] = [];
+
+for (let i = 0; i < 9; i++) {
+    if (i % 2 == 0) {
+        persons.push(
+            newStudent({name: names[i], school: 'First Boring School or so'})
+        ); 
+    } else {
+        persons.push(
+            newWorker(names[i], workplaces[Math.round(Math.random() * 2)].id)
+        );
+    }
+}
+
+console.log({persons});
